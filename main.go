862a4f32bb51e730d8ae7136219d25ffc6968887
Rahul Kumar <rahul.kr.7370@gmail.com>
2023-01-15 21:07:19 +0530

    added crud

--- main.go
+++ main.go
@@ -1,49 +1,110 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
-//monorepo
-type Book struct {
-	Title  string `json:"booktitle"`
-	Author string `json:"author-1"`
-	Page   int
-	Price  float32
+type PostPayload struct {
+	Name string `json:"name"`
 }
 
 func main() {
-	// book := Book{}
-	// book.Title = "Game of thrones"
-	// book.Author = "Ashish"
-	// book.Page = 1000
-	// book.Price = 2.5
-
-	// //book object -> book.json
-	// // Marshal ====  Golang object ---> byteArray --> write in a file or print(that will be in json format)
-	// byteArray, err := json.Marshal(book)
-	// if err != nil {
-	// 	log.Fatal("got error", err)
+	// post := map[string]string{
+	// 	"rahul": "jkghvkjhf",
 	// }
-	// log.Println(string(byteArray))
+	// r := gin.Default() //application will be configured on port 8080
+	// pingFunc := func(c *gin.Context) {
 
-	// json -> book object
-	// Unmarshal ==> data-->bytearray----> golang struct
-	book2 := Book{}
-	//jsondata := `{"booktitle":"some title","author-1":"Ashish","Page":1000,"Price":2.5}`
+	// 	key := map[string]any{
+	// 		"ping": "pingu",
+	// 	}
+	// 	c.Keys = key
+	// 	res := gin.H{
+	// 		"message":      "pong",
+	// 		"contextvalue": c.Keys["ping"],
+	// 	}
+	// 	c.JSON(http.StatusUnauthorized, res)
+	// }
+	// pongFunc := func(c *gin.Context) {
+	// 	res := gin.H{
+	// 		"message":      "pong",
+	// 		"contextvalue": c.Keys["ping"],
+	// 	}
+	// 	log.Println(c.Keys["ping"])
+	// 	c.JSON(http.StatusUnauthorized, res)
+	// }
+	// dingFunc := func(c *gin.Context) {
+	// 	res := gin.H{
+	// 		"message": "pong",
+	// 	}
+	// 	c.JSON(http.StatusUnauthorized, res)
+	// }
+	// dongFunc := func(c *gin.Context) {
+	// 	res := gin.H{
+	// 		"message": "pong",
+	// 	}
+	// 	c.JSON(http.StatusUnauthorized, res)
+	// }
+	post := map[string]string{
+		"rahul": "Present",
+	}
+	r := gin.Default()
+	getPost := func(c *gin.Context) {
+		res := gin.H{
+			"message": post,
+		}
+		c.JSON(http.StatusUnauthorized, res)
+	}
 
-	fileDataArr, err := ioutil.ReadFile("./book.json")
-	if err != nil {
-		log.Fatal("got error while ReadFile", err)
+	// r.GET("/ping", pingFunc) //http://localhost:8080/ping
+	// r.GET("/pong", pongFunc)
+	// r.GET("/ding", dingFunc)
+	// r.GET("/dong", dongFunc)
+	r.GET("/post", getPost)
+	createPost := func(c *gin.Context) {
+		//log.Println("*********", c, "*********")
+		var p1 PostPayload
+		err := c.ShouldBindJSON(&p1)
+		if err != nil {
+			log.Print("got error", err)
+		}
+		post[p1.Name] = "Present"
+		res := gin.H{
+			"message": p1,
+		}
+		c.JSON(http.StatusOK, res)
 	}
-	//log.Println(string(fileDataArr))
-	jsondata := string(fileDataArr)
-	err = json.Unmarshal([]byte(jsondata), &book2)
-	if err != nil {
-		log.Fatal("got error while Unmarshal", err)
+	r.POST("/post", createPost)
+	deletePost := func(c *gin.Context) {
+
+		//key := c.Params.ByName("id")
+		key := c.Query("id")
+		//to delete the key value pair from a map via key
+		delete(post, key)
+		res := gin.H{
+			"message": key + " Deleted",
+		}
+		c.JSON(http.StatusOK, res)
 	}
-	fmt.Println(book2) //some title
+	//r.DELETE("/post/:id", deletePost)
+	r.DELETE("/post", deletePost)
+	//8:17
+	// r.PUT("/post", updatePost)       //data will be sent via payload and query
+	// r.PATCH("/post", updateNamebyId) //data will be sent via query
+	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run()
 }
+
+/// delete post by id:-
+// url: http://localhost:8080/post?id=rahul
+// url: http://localhost:8080/post/rahul
+// request method: delete
+
+// get post by id:-
+// url: http://localhost:8080/post?id=1
+// request method: get
+
+//http://localhost:8080/post
