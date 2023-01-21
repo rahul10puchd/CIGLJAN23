c60acc85641e399068187d91dd9ab14b2f7d2d28
Rahul Kumar <rahul.kr.7370@gmail.com>
2023-01-21 10:04:50 +0530

    app schema

--- main.go
+++ main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"gin/basic/db"
+	"gin/basic/router"
 	"log"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
-type PostPayload struct {
-	Name string `json:"name"`
-}
-
 func main() {
+	r := gin.Default()
+	_, err := db.GetPostgresDBConnection()
+	if err != nil {
+		log.Fatal("Unable to establish DB connection", err)
+	} else {
+		log.Panicln("DB connection establsihed")
+	}
+
+	router.PostRouter(r)
+	router.UserRouter(r)
+	r.Run()
+
 	// post := map[string]string{
 	// 	"rahul": "jkghvkjhf",
 	// }
@@ -48,56 +57,30 @@ func main() {
 	// 	}
 	// 	c.JSON(http.StatusUnauthorized, res)
 	// }
-	post := map[string]string{
-		"rahul": "Present",
-	}
-	r := gin.Default()
-	getPost := func(c *gin.Context) {
-		res := gin.H{
-			"message": post,
-		}
-		c.JSON(http.StatusUnauthorized, res)
-	}
 
+	// r := gin.Default()
+
+	// router.Router(context.Background(), r)
+	// r.Run()
 	// r.GET("/ping", pingFunc) //http://localhost:8080/ping
 	// r.GET("/pong", pongFunc)
 	// r.GET("/ding", dingFunc)
 	// r.GET("/dong", dongFunc)
-	r.GET("/post", getPost)
-	createPost := func(c *gin.Context) {
-		//log.Println("*********", c, "*********")
-		var p1 PostPayload
-		err := c.ShouldBindJSON(&p1)
-		if err != nil {
-			log.Print("got error", err)
-		}
-		post[p1.Name] = "Present"
-		res := gin.H{
-			"message": p1,
-		}
-		c.JSON(http.StatusOK, res)
-	}
-	r.POST("/post", createPost)
-	deletePost := func(c *gin.Context) {
+	//r.GET("/post", getPost)
 
-		//key := c.Params.ByName("id")
-		key := c.Query("id")
-		//to delete the key value pair from a map via key
-		delete(post, key)
-		res := gin.H{
-			"message": key + " Deleted",
-		}
-		c.JSON(http.StatusOK, res)
-	}
+	// r.POST("/post", createPost)
+
 	//r.DELETE("/post/:id", deletePost)
-	r.DELETE("/post", deletePost)
+	// r.DELETE("/post", deletePost)
 	//8:17
 	// r.PUT("/post", updatePost)       //data will be sent via payload and query
 	// r.PATCH("/post", updateNamebyId) //data will be sent via query
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	r.Run()
+
 }
 
+// backend app: language, docker, kubes, git, database, cloud, ci/cd(github workflow)
+// adv: libs for diffrent(store pass-hashicorp vault)
 /// delete post by id:-
 // url: http://localhost:8080/post?id=rahul
 // url: http://localhost:8080/post/rahul
